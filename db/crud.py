6144e929e6283aa2286a44c300cf900be6654352
Pekmanchik <pekermanchik@gmail.com>
2025-02-23 12:39:00 +0200

    db added

--- db/crud.py
+++ db/crud.py
@@ -13,16 +13,14 @@ class UsersService:
     async def add_new_user(
             tg_id: int,
             tg_name: Optional[str],
-            lang: str,
-            utm: Optional[str]
+            lang: str
             ) -> None:
         async with async_session_maker() as session:
             try:
                 new_user = Users(
                     tg_id=tg_id,
                     tg_name=tg_name,
-                    lang=lang,
-                    utm=utm
+                    lang=lang
                     )
                 session.add(new_user)
                 await session.commit()
@@ -32,7 +30,7 @@ class UsersService:
 
 
     @staticmethod
-    async def check_exist_user(tg_id: int) ->bool:
+    async def check_exist_user(tg_id: int) -> bool:
         async with async_session_maker() as session:
             result = await session.execute(
                 select(Users).where(Users.tg_id == tg_id)
