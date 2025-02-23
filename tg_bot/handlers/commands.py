9c37bc5dd84c0ab4330f4ecca214511f97e1727e
Pekmanchik <pekermanchik@gmail.com>
2025-02-23 12:38:47 +0200

    commands updated

---
+++ tg_bot/handlers/commands.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import F, Router, types
+from aiogram.fsm.context import FSMContext
+from aiogram.types import (
+    FSInputFile,
+    Message
+)
+from aiogram.filters import Command
+from typing import Dict
+
+router = Router()
+
+
+@router.message(Command('start'))
+async def send_welcome(message: types.Message, **kwargs):
+    print(f"Количество аргументов в kwargs: {len(kwargs)}")
+    #user_status = kwargs[user_status]
+    await message.reply(text="Привет, Администратор!")
+    # user_status = user_data['user_status']
+    # if user_status == 'admin':
+    #     await message.reply(text="Привет, Администратор!")
